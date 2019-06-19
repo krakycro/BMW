77772b4dc6ff0bde4bd996bdbf753cf47b262a2b
Filip Kraus (EXTERN) <extern.fillip.kraus@tttech-automotive.com>
2019-06-19 09:02:59 -0700

    Initial commit with a starting workplace

---
+++ src/python/main.py
@@ -0,0 +1,4 @@
+from src.lib.libstuff import *
+
+def gogo(var1, var2):
+	return var1 + var2
---
+++ test/test_1.py
@@ -0,0 +1,7 @@
+from src.lib.libstuff import *
+
+from src.python.main import *
+
+assert(gogo(1,3) == 4)
+
+
